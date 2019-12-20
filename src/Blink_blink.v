cbcf847fa4fcdc884bd907833ab8b035c47bfb26
yameholo <ame.aroid.xz5@gmail.com>
2019-12-21 01:17:12 +0900

    Update

--- src/Blink_blink.v
+++ src/Blink_blink.v
@@ -6,61 +6,64 @@ module Blink_blink
   );
 
   //localparams
-  localparam Blink_blink_main_bz_b1_INIT = 0;
-  localparam Blink_blink_main_bz_b1_S1 = 1;
-  localparam Blink_blink_main_bz_b1_FINISH = 2;
-  localparam Blink_blink_main_bz_L1_while2_S0 = 3;
-  localparam Blink_blink_main_bz_L1_while2_S1 = 4;
-  localparam Blink_blink_main_bz_L1_forelse6_S0 = 5;
-  localparam Blink_blink_main_bz_L1_forelse6_S1 = 6;
-  localparam Blink_blink_main_bz_L2_fortest4_S0 = 7;
-  localparam Blink_blink_main_bz_L2_forbody5_S0 = 8;
-  localparam Blink_blink_main_bz_L2_forbody5_S1 = 9;
+  localparam Blink_blink_main_c0_b1_INIT = 0;
+  localparam Blink_blink_main_c0_b1_S1 = 1;
+  localparam Blink_blink_main_c0_b1_FINISH = 2;
+  localparam Blink_blink_main_c0_L1_while2_S0 = 3;
+  localparam Blink_blink_main_c0_L1_while2_S1 = 4;
+  localparam Blink_blink_main_c0_L1_forelse6_S0 = 5;
+  localparam Blink_blink_main_c0_L1_forelse6_S1 = 6;
+  localparam Blink_blink_main_c0_L2_fortest4_S0 = 7;
+  localparam Blink_blink_main_c0_L2_forbody5_S0 = 8;
+  localparam Blink_blink_main_c0_L2_forbody5_S1 = 9;
   
   //signals: 
-  wire blink_main_bz_c532_inl1;
-  wire blink_main_bz_led_bit3;
-  reg blink_main_bz_led_bit2;
-  reg        [3:0] blink_main_bz_state;
-  wire signed [31:0] blink_main_bz_i_inl14;
-  wire signed [31:0] blink_main_bz_t530_inl1;
+  wire blink_main_c0_c537_inl2;
+  wire blink_main_c0_led_bit3;
+  reg blink_main_c0_led_bit2;
+  reg        [3:0] blink_main_c0_state;
+  wire signed [31:0] blink_main_c0_i_inl24;
+  wire signed [31:0] blink_main_c0_switch_result1;
+  wire signed [31:0] blink_main_c0_t535_inl2;
   wire signed [31:0] interval;
-  reg signed [31:0] blink_main_bz_i_inl13;
+  reg signed [31:0] blink_main_c0_i_inl23;
   //combinations: 
-  assign blink_main_bz_c532_inl1 = (blink_main_bz_i_inl13 < blink_main_bz_t530_inl1);
-  assign blink_main_bz_i_inl14 = (blink_main_bz_i_inl13 + 1);
-  assign blink_main_bz_led_bit3 = (1 - blink_main_bz_led_bit2);
-  assign blink_main_bz_t530_inl1 = (interval / 2);
+  assign blink_main_c0_c537_inl2 = (blink_main_c0_i_inl23 < blink_main_c0_t535_inl2);
+  assign blink_main_c0_i_inl24 = (blink_main_c0_i_inl23 + 1);
+  assign blink_main_c0_led_bit3 = blink_main_c0_switch_result1;
+  assign blink_main_c0_switch_result1 = (1 - blink_main_c0_led_bit2);
+  assign blink_main_c0_t535_inl2 = (interval / 2);
   assign interval = 16;
   
   always @(posedge clk) begin
     if (rst) begin
-      blink_main_bz_i_inl13 <= 0;
-      blink_main_bz_led_bit2 <= 0;
+      blink_main_c0_i_inl23 <= 0;
+      blink_main_c0_led_bit2 <= 0;
       led <= 0;
-      blink_main_bz_state <= Blink_blink_main_bz_b1_INIT;
+      blink_main_c0_state <= Blink_blink_main_c0_b1_INIT;
     end else begin //if (rst)
-      case(blink_main_bz_state)
-      Blink_blink_main_bz_b1_INIT: begin
-        blink_main_bz_led_bit2 <= 1;
-        blink_main_bz_state <= Blink_blink_main_bz_L1_while2_S0;
+      case(blink_main_c0_state)
+      Blink_blink_main_c0_b1_INIT: begin
+        blink_main_c0_led_bit2 <= 1;
+        blink_main_c0_state <= Blink_blink_main_c0_L1_while2_S0;
       end
-      Blink_blink_main_bz_L1_while2_S0: begin
-        led <= blink_main_bz_led_bit2;
-        /* blink_main_bz_led_bit3 <= (1 - blink_main_bz_led_bit2); */
-        /* blink_main_bz_t530_inl1 <= (interval / 2); */
-        blink_main_bz_i_inl13 <= 0;
-        blink_main_bz_state <= Blink_blink_main_bz_L2_fortest4_S0;
+      Blink_blink_main_c0_L1_while2_S0: begin
+        led <= blink_main_c0_led_bit2;
+        /* blink_main_c0_switch_result1 <= (1 - blink_main_c0_led_bit2); */
+        /* blink_main_c0_t535_inl2 <= (interval / 2); */
+        blink_main_c0_i_inl23 <= 0;
+        /* blink_main_c0_led_bit3 <= blink_main_c0_switch_result1; */
+        blink_main_c0_state <= Blink_blink_main_c0_L2_fortest4_S0;
       end
-      Blink_blink_main_bz_L2_fortest4_S0: begin
-        /* blink_main_bz_c532_inl1 <= (blink_main_bz_i_inl13 < blink_main_bz_t530_inl1); */
-        if (blink_main_bz_c532_inl1) begin
-          /* blink_main_bz_i_inl14 <= (blink_main_bz_i_inl13 + 1); */
-          blink_main_bz_i_inl13 <= blink_main_bz_i_inl14;
-          blink_main_bz_state <= Blink_blink_main_bz_L2_fortest4_S0;
+      Blink_blink_main_c0_L2_fortest4_S0: begin
+        /* blink_main_c0_c537_inl2 <= (blink_main_c0_i_inl23 < blink_main_c0_t535_inl2); */
+        if (blink_main_c0_c537_inl2) begin
+          /* blink_main_c0_i_inl24 <= (blink_main_c0_i_inl23 + 1); */
+          blink_main_c0_i_inl23 <= blink_main_c0_i_inl24;
+          blink_main_c0_state <= Blink_blink_main_c0_L2_fortest4_S0;
         end else begin
-          blink_main_bz_led_bit2 <= blink_main_bz_led_bit3;
-          blink_main_bz_state <= Blink_blink_main_bz_L1_while2_S0;
+          blink_main_c0_led_bit2 <= blink_main_c0_led_bit3;
+          blink_main_c0_state <= Blink_blink_main_c0_L1_while2_S0;
         end
       end
       endcase
--- src/blink.py
+++ src/blink.py
@@ -2,6 +2,14 @@ from polyphony import testbench, module, is_worker_running
 from polyphony.io import Port
 from polyphony.typing import bit
 
+
+ON = 1
+
+
+def switch(data_bit:bit) -> bit:
+    return 1 - data_bit
+
+
 @module
 class Blink:
     def __init__(self, interval):
@@ -10,14 +18,16 @@ class Blink:
         self.append_worker(self.main)
 
     def main(self):
-        led_bit:bit = 1
+        led_bit:bit = ON
         while is_worker_running():
             self.led(led_bit)
-            led_bit= 1 - led_bit
+            led_bit = switch(led_bit)
             self._wait()
 
     def _wait(self):
         for i in range(self.interval // 2):
             pass
 
-blink = Blink(16)
+
+blink = Blink(20 - 4)
+
